be9d024c83c836b673ec6bb46b3582f71be926ee
Joan Montoya <joan15montoya@gmail.com>
2022-06-23 00:57:06 -0500

    feat: sidebar creation, web styling completion

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ function App() {
     const [result, setResult] = React.useState([]);
     const [poke, setPoke] = React.useState([]);
     const [load, setLoad] = React.useState('true');
-    const [counter, setCounter] = useState(0);
+    const [info, setInf] = React.useState('true');
     const [nombre, setNombre] = useState("");
     const [imagen, setImage] = useState("");
+    const [imagenT, setImageT] = useState("");
+    const [color, setColor] = useState("");
     const [pokearr, setPokA] = React.useState([]);
     const [inicio, setIn] = useState(0);
     const [Final, setFin] = useState(9);
+    const [posicion, setPo] = useState(0);
+
 
     const arr = [];
 
@@ -29,9 +33,18 @@ function App() {
 
     setTimeout(() => {
         setLoad(false);
+        document.body.style.backgroundColor = '#a5e7fa';
     }, 1000);
 
     function llamar(){
+      if(Final >= 199){
+        setFin(9);
+        setIn(0);
+      }
+      if(inicio < 0){
+        setFin(9);
+        setIn(0);
+      }
       if(poke.length >= 90){
         if(pokearr.length <= 9){
           for(var i = inicio; i <= Final; i++){
@@ -45,64 +58,61 @@ function App() {
       console.log(pokearr)
     }
 
-    // function Nombre(){
-    //   for(var i = 0; i <= 10; i++){
-    //     pokearr.push(poke[i]);
-    //   }
-    //   console.log(pokearr);
-    //   setNombre(pokearr[counter].name);
-    //   setImage(pokearr[counter].sprites.other.dream_world.front_default);
-    // }
-
-      // <img className='imagen' src={imagen} width="300px" height="300px"></img>
-
-      // <h1> {nombre}</h1>
-
-      
-      // <button className='boton' onClick={() => {
-      //   setCounter(counter = counter - 1); Nombre();
-      // }}><h3>Anterior</h3></button>
-      // <button className='boton' onClick={() => {
-      //   setCounter(counter + 1); Nombre();
-      // } }><h3>Siguiente</h3></button> 
+    function barral(val){
+      setImage(pokearr[val].sprites.other.dream_world.front_default);
+      setImageT(pokearr[val].sprites.back_default);
+      setNombre(pokearr[val].name.toUpperCase());
+      setColor(pokearr[val].types[0].type.name);
+      setPo(val);
+    }
       return (
         <div className="App">
            <div className='pokegallery'>
+           <h1>POKEDEX POKE API</h1>
+           
             {llamar()}
             { load ? (
               <p>Loading...</p>
             ) :  (
+
+              
     
               pokearr.map((img, i) => (
-                <div className='card' id={img.id} key={img.id}>
-                  <h3>id: {pokearr[i].id} - <b>{pokearr[i].name}</b></h3>
+                <div className='card' id={pokearr[i].types[0].type.name} key={img.id} name={pokearr[i].types[0].type.name} onClick={() => {setInf(false);barral(i)}}>
+                    <p className='ids'>Id: {pokearr[i].id} </p>
+                    <p className='ids1'>Type: {pokearr[i].types[0].type.name.toUpperCase()}</p>
+                  <br></br>
+                  <h3><b>{pokearr[i].name.toUpperCase()}</b></h3>
                   <img className='imagen' src={pokearr[i].sprites.other.dream_world.front_default}/>
-                  <img className='imagen' src={pokearr[i].sprites.back_default}/>
-                    <div className='container'>
-                      <p><b>Xp base: </b>{pokearr[i].base_experience}</p>
-                      <p className='habilidades'><b>Movimientos</b> <br></br>  
-                      {pokearr[i].moves[0].move.name}, {pokearr[i].moves[1].move.name} <br></br>
-                      {pokearr[i].moves[2].move.name}, {pokearr[i].moves[3].move.name}
-                      {/* {pokearr[i].abilities[0].ability.name}, {pokearr[i].abilities[1].ability.name} */} 
-                      </p> 
-                      <div className={pokearr[i].types[0].type.name}>
-                        <p className='habilidades'>{pokearr[i].types[0].type.name}</p> 
-                      </div>
-                      
-                  </div>
                 </div>
-
-                
               ))
             )}
-    
-    
-    
-    </div>
-    <button className='boton' onClick={() => {setIn(inicio - 9); setFin(Final - 9);} }><h3>Atras</h3></button>
-    <button className='boton' onClick={() => {setIn(inicio + 10); setFin(Final + 10);} }><h3>Siguiente</h3></button>
-
-        </div>
+          </div>
+          <div className='barralateral'>
+            {info ?
+              <p>Loading...</p> : 
+              <div className={color}>
+                <img className='icono' src={imagen}/>
+                <img className='imagenb' src={imagen}/>
+                  <br></br>
+                  <img className='imagenb1' src={imagenT}/>
+                  <div className='info'>
+                    <div className='informacion'>
+                      <h1>{nombre}</h1>
+                        <p><b>Xp base: </b>{pokearr[posicion].base_experience}</p>
+                        <p className='habilidades'> Type: {pokearr[posicion].types[0].type.name}</p>
+                        <p><b>Movimientos</b> <br></br>  
+                          {pokearr[posicion].moves[0].move.name}, {pokearr[posicion].moves[1].move.name} <br></br>
+                          {pokearr[posicion].moves[2].move.name}, {pokearr[posicion].moves[3].move.name}
+                        </p> 
+                    </div>
+                  </div>
+              </div>
+            }
+          </div>
+            <div className='botona' onClick={() => {setIn(inicio - 9); setFin(Final - 9);} }><h3 className='order'>Atras</h3></div>
+            <div className='botons' onClick={() => {setIn(inicio + 10); setFin(Final + 10);} }><h3 className='order'>Siguiente</h3></div>
+          </div>
       );
     }
     
